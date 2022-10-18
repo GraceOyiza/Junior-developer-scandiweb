356c5156a6ea40971944724155537d56c29e7d7e
GraceOyiza <graceoyiza13@gmail.com>
2022-10-18 21:39:39 +0100

    Resole warnings

--- src/components/ThumbNail.js
+++ src/components/ThumbNail.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { ImageThumbNail } from '../styled/ImageThumbnail';
 
 class ThumbNail extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+ 
   render() {
     return <div>
         <ImageThumbNail data-index={this.props.index} src={this.props.src} />
